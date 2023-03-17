63e1bd0199e8dfdda376605a0993d6ff5dd8fafd
gitlab-runner <do-not-reply@cypress.com>
2023-03-17 11:49:59 -0500

    Upload mtb-example-btsdk-ble-hello-sensor-20736 4.2.0.11524 [916]

--- hello_sensor.c
+++ hello_sensor.c
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2022, Cypress Semiconductor Corporation (an Infineon company) or
+ * Copyright 2016-2023, Cypress Semiconductor Corporation (an Infineon company) or
  * an affiliate of Cypress Semiconductor Corporation.  All rights reserved.
  *
  * This software, including source code, documentation and related
@@ -65,6 +65,7 @@
 #include "gpiodriver.h"
 #include "string.h"
 #include "stdio.h"
+#include "stdint.h"
 #include "platform.h"
 #include "hello_sensor.h"
 #include "sparcommon.h"
